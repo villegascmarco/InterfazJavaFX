b768c7d7b0695460ed3aab952c1f16d0772b466b
sebastianSanchez23 <56986944+sebastianSanchez23@users.noreply.github.com>
2019-11-04 08:39:57 -0600

    Se agregó el Main actual

---
+++ src/edu/softech/InterfazJavaFX/gui/WindowMain.java
@@ -0,0 +1,38 @@
+package edu.softech.InterfazJavaFX.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class WindowMain extends Application {
+
+    private static final Image IMG_LOGO = new Image(System.class.getResource("/res/lotus.png").toString());
+
+    FXMLLoader fxmll;
+
+    Stage window;
+
+    Scene scene;
+
+    public WindowMain() {
+        fxmll = new FXMLLoader(System.class.getResource("/edu/softech/"
+                + "InterfazJavaFX/gui/fxml/window_main.fxml"));
+        //  fxmll.setController(this); Quitamos esta linea para crear un controlador para cada seccion
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        fxmll.load();
+        scene = new Scene(fxmll.getRoot());
+        window = primaryStage;
+        window.setScene(scene);
+
+        window.setTitle("MySpa");
+        window.getIcons().add(IMG_LOGO);
+        window.show();
+
+    }
+
+}
