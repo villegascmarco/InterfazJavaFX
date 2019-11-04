9b4f7c7e521297c3e2b091a821e287bb604cd963
sebastianSanchez23 <56986944+sebastianSanchez23@users.noreply.github.com>
2019-11-04 08:34:44 -0600

    Se eliminó menú lateral

--- src/edu/softech/InterfazJavaFX/gui/WindowSucursales.java
+++ src/edu/softech/InterfazJavaFX/gui/WindowSucursales.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.dsm401.examen.gui;
+package edu.softech.InterfazJavaFX.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -22,17 +22,7 @@ import javafx.stage.Stage;
  */
 public class WindowSucursales extends Application {
 
-//    private static final Image IMG_YOGA = new Image(System.class.getResource("/res/lotus.png").toString());
     private static final Image IMG_LOGO = new Image(System.class.getResource("/res/lotus.png").toString());
-//    private static final Image IMG_SUCURSALES = new Image(System.class.getResource("/res/sucursal.png").toString());
-//    private static final Image IMG_EMPLEADOS = new Image(System.class.getResource("/res/empleado.png").toString());
-//    private static final Image IMG_CLIENTES = new Image(System.class.getResource("/res/cliente.png").toString());
-//    private static final Image IMG_PRODUCTOS = new Image(System.class.getResource("/res/productos.png").toString());
-//    private static final Image IMG_SERVICIOS = new Image(System.class.getResource("/res/servicios.png").toString());
-//    private static final Image IMG_RESERVACIONES = new Image(System.class.getResource("/res/reservaciones.png").toString());
-//    private static final Image IMG_SALAS = new Image(System.class.getResource("/res/salas.png").toString());
-//    private static final Image IMG_TRATAMIENTO = new Image(System.class.getResource("/res/tratamientos.png").toString());
-//    private static final Image IMG_CTRLSALA = new Image(System.class.getResource("/res/ctrlSala.png").toString());
     private static final Image IMG_SUCURSAL = new Image(System.class.getResource("/res/yoga.png").toString());
     private static final Image IMG_LOCALIZACION = new Image(System.class.getResource("/res/localizacion.png").toString());
 
@@ -81,33 +71,11 @@ public class WindowSucursales extends Application {
         scene = new Scene(fxmll.getRoot());
         window = primaryStage;
         window.setScene(scene);
-//        window.setR{esizable(false);
-//        window.resizableProperty().setValue(false);
         window.setTitle("MySpa");
         window.getIcons().add(IMG_LOGO);
         window.show();
 
-//        imgvLogo.setImage(IMG_YOGA);
 
-//        btnSucursales.setContentDisplay(ContentDisplay.RIGHT);
-//        btnEmpleados.setContentDisplay(ContentDisplay.RIGHT);
-//        btnClientes.setContentDisplay(ContentDisplay.RIGHT);
-//        btnProductos.setContentDisplay(ContentDisplay.RIGHT);
-//        btnServicios.setContentDisplay(ContentDisplay.RIGHT);
-//        btnReservaciones.setContentDisplay(ContentDisplay.RIGHT);
-//        btnSalas.setContentDisplay(ContentDisplay.RIGHT);
-//        btnTratamientos.setContentDisplay(ContentDisplay.RIGHT);
-//        btnCtrlSala.setContentDisplay(ContentDisplay.RIGHT);
-
-//        btnSucursales.setGraphic(new ImageView(IMG_SUCURSALES));
-//        btnEmpleados.setGraphic(new ImageView(IMG_EMPLEADOS));
-//        btnClientes.setGraphic(new ImageView(IMG_CLIENTES));
-//        btnProductos.setGraphic(new ImageView(IMG_PRODUCTOS));
-//        btnServicios.setGraphic(new ImageView(IMG_SERVICIOS));
-//        btnReservaciones.setGraphic(new ImageView(IMG_RESERVACIONES));
-//        btnSalas.setGraphic(new ImageView(IMG_SALAS));
-//        btnTratamientos.setGraphic(new ImageView(IMG_TRATAMIENTO));
-//        btnCtrlSala.setGraphic(new ImageView(IMG_CTRLSALA));
         imgvSucursal.setImage(IMG_SUCURSAL);
         imgSucursal.setImage(IMG_SUCURSAL);
         imgvLocalizacion.setImage(IMG_LOCALIZACION);
