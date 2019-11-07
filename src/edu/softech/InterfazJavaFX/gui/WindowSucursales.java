e8a60c07ea30419c7c7397c500f07551892fb93a
sebastianSanchez23 <56986944+sebastianSanchez23@users.noreply.github.com>
2019-11-06 19:20:59 -0600

    Se cambió el diseño

--- src/edu/softech/InterfazJavaFX/gui/WindowSucursales.java
+++ src/edu/softech/InterfazJavaFX/gui/WindowSucursales.java
@@ -8,12 +8,12 @@ package edu.softech.InterfazJavaFX.gui;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -22,48 +22,20 @@ import javafx.stage.Stage;
  */
 public class WindowSucursales extends Application {
 
-    private static final Image IMG_LOGO = new Image(System.class.getResource("/res/lotus.png").toString());
     private static final Image IMG_SUCURSAL = new Image(System.class.getResource("/res/yoga.png").toString());
     private static final Image IMG_LOCALIZACION = new Image(System.class.getResource("/res/localizacion.png").toString());
 
     FXMLLoader fxmll;
     Stage window;
     Scene scene;
-    @FXML
-    ImageView imgvLogo;
-    @FXML
-    Button btnSucursales;
-    @FXML
-    Button btnEmpleados;
-    @FXML
-    Button btnClientes;
-    @FXML
-    Button btnProductos;
-    @FXML
-    Button btnServicios;
-    @FXML
-    Button btnReservaciones;
-    @FXML
-    Button btnSalas;
-    @FXML
-    Button btnTratamientos;
-    @FXML
-    Button btnCtrlSala;
+
     @FXML
     ImageView imgvSucursal;
     @FXML
     ImageView imgSucursal;
     @FXML
     ImageView imgvLocalizacion;
-    @FXML
-    ComboBox cmbEstatus;
-    @FXML
-    ComboBox cmbEstatusMod;
-
-    public WindowSucursales() {
-        fxmll = new FXMLLoader(System.class.getResource("/edu/dsm401/examen/gui/fxml/window_sucursales.fxml"));
-        fxmll.setController(this);
-    }
+   
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -72,19 +44,17 @@ public class WindowSucursales extends Application {
         window = primaryStage;
         window.setScene(scene);
         window.setTitle("MySpa");
-        window.getIcons().add(IMG_LOGO);
+        window.setMaximized(true);
+        window.setResizable(true);
+        
         window.show();
-
-
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        System.out.println(screenBounds);
+        
         imgvSucursal.setImage(IMG_SUCURSAL);
         imgSucursal.setImage(IMG_SUCURSAL);
         imgvLocalizacion.setImage(IMG_LOCALIZACION);
-        llenarComboBox();
-    }
-
-    private void llenarComboBox() {
-        cmbEstatus.getItems().addAll("Activa", "Inactiva");
-        cmbEstatusMod.getItems().addAll("Activa", "Inactiva");
 
     }
+
 }
