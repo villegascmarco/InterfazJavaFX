dde59662be65b54d77f66ef10d95d943969297d2
sebastianSanchez23 <56986944+sebastianSanchez23@users.noreply.github.com>
2019-11-30 13:58:58 -0600

    Se actualizaron los métodos para sucursales

--- src/edu/softech/InterfazJavaFX/api/Api.java
+++ src/edu/softech/InterfazJavaFX/api/Api.java
@@ -1,34 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.softech.InterfazJavaFX.api;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import edu.softech.MySpa.modelo.Cliente;
-import edu.softech.MySpa.modelo.Usuario;
+import edu.softech.MySpa.modelo.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Villegas
- */
 public final class Api {
 
-    private static final String RUTA = "http://localhost:8084/MySpa/api/";
+    private static final String RUTA = "http://localhost:8080/MySpa/api/";
 
     int respuestaServidor = 0;
 
@@ -39,6 +28,8 @@ public final class Api {
     private String lineaActual = null;
 
     private JsonParser parser = null;
+
+    private String contenidoRespuesta = null;
 
     private URL url;
 
@@ -92,7 +83,7 @@ public final class Api {
         }
         try {
             JsonElement json = parser.parse(hacerPeticion(enlace, opcion));
-            
+
             return json;
 
         } catch (MalformedURLException ex) {
@@ -192,6 +183,145 @@ public final class Api {
 
         connHttp.disconnect();
         return contenidoRespuesta;
+
+    }
+
+    public JsonObject eliminarSucursal(String objeto) throws Exception {
+        parser = new JsonParser();
+        JsonObject json = null;
+        String acum = "";
+
+        for (int i = 0; i < objeto.length(); i++) {
+            if (objeto.charAt(i) == ' ') {
+                acum += "%20";
+            } else {
+                acum += objeto.charAt(i);
+            }
+        }
+
+        String ruta = RUTA + acum;//http://localhost:8080/MySpa/api/sucursal?
+        URL url = new URL(ruta);
+
+        HttpURLConnection connHttp = (HttpURLConnection) url.openConnection();
+        connHttp.setDoOutput(true);
+        connHttp.setRequestMethod("DELETE");
+        connHttp.setRequestProperty("datatype", "json");
+
+        respuestaServidor = connHttp.getResponseCode();
+
+        if (respuestaServidor == HttpURLConnection.HTTP_OK) {
+
+            isr = new InputStreamReader(connHttp.getInputStream());
+            br = new BufferedReader(isr);
+
+            contenidoRespuesta = "";
+
+            while ((lineaActual = br.readLine()) != null) {
+                contenidoRespuesta += lineaActual;
+            }
+
+            br.close();
+
+            connHttp.disconnect();
+
+            json = parser.parse(contenidoRespuesta).getAsJsonObject();
+
+        }
+
+        return json;
+    }
+
+    //METODO PARA ACTUALIZAR UNA SUCURSAL MEDIANTE METODO PUT
+    public JsonObject actualizarSucursal(String objeto) throws Exception {
+        parser = new JsonParser();
+
+        JsonObject json = null;
+        String acum = "";
+        for (int i = 0; i < objeto.length(); i++) {
+            if (objeto.charAt(i) == ' ') {
+                acum += "%20";
+            } else {
+                acum += objeto.charAt(i);
+            }
+        }
+
+        String ruta = RUTA + acum;//http://localhost:8080/MySpa/api/sucursal?
+        URL url = new URL(ruta);
+
+        HttpURLConnection connHttp = (HttpURLConnection) url.openConnection();
+        connHttp.setDoOutput(true);
+        connHttp.setRequestMethod("PUT");
+        connHttp.setRequestProperty("datatype", "json");
+
+        respuestaServidor = connHttp.getResponseCode();
+
+        if (respuestaServidor == HttpURLConnection.HTTP_OK) {
+
+            isr = new InputStreamReader(connHttp.getInputStream());
+            br = new BufferedReader(isr);
+
+            contenidoRespuesta = "";
+
+            while ((lineaActual = br.readLine()) != null) {
+                contenidoRespuesta += lineaActual;
+            }
+
+            br.close();
+
+            connHttp.disconnect();
+
+            json = parser.parse(contenidoRespuesta).getAsJsonObject();
+
+        }
+
+        return json;
+
+    }
+
+    //METODO PARA INSERTAR UNA SUCURSAL MEDIANTE EL METODO POST
+    public JsonObject insertarSucursal(String objeto) throws Exception {
+        parser = new JsonParser();
+
+        JsonObject json = null;
+        String acum = "";
+        for (int i = 0; i < objeto.length(); i++) {
+            if (objeto.charAt(i) == ' ') {
+                acum += "%20";
+            } else {
+                acum += objeto.charAt(i);
+            }
+        }
+
+        String ruta = RUTA + acum;//http://localhost:8080/MySpa/api/sucursal?
+        URL url = new URL(ruta);
+
+        HttpURLConnection connHttp = (HttpURLConnection) url.openConnection();
+        connHttp.setDoOutput(true);
+        connHttp.setRequestMethod("POST");
+        connHttp.setRequestProperty("Content-Type", "application/json");
+
+        respuestaServidor = connHttp.getResponseCode();
+
+        if (respuestaServidor == HttpURLConnection.HTTP_OK) {
+
+            isr = new InputStreamReader(connHttp.getInputStream());
+            br = new BufferedReader(isr);
+
+            contenidoRespuesta = "";
+
+            while ((lineaActual = br.readLine()) != null) {
+                contenidoRespuesta += lineaActual;
+            }
+
+            br.close();
+
+            connHttp.disconnect();
+
+            json = parser.parse(contenidoRespuesta).getAsJsonObject();
+
+        }
+
+        return json;
 
     }
 
